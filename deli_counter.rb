aaf8f3438512138ef1794e2c379caa336e28e71d
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-16 20:21:00 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def line(array)
+  if array.length < 1
+    puts "The line is currently empty."
+  elsif array.length > 1
+    num = 1
+    for i in array
+      i.prepend(" #{num}. ")
+      num+=1
+    end
+    line = array.join
+    puts "The line is currently:#{line}"
+  end
+end
+
+def take_a_number(array, name)
+  if array.length < 1
+    array.unshift("#{name}")
+  elsif array.length >= 1
+    array.push("#{name}")
+  end
+   puts "Welcome, #{name}. You are number " + (array.index(name) + 1).to_s + " in line."
+end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  elsif array.length >= 1
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
